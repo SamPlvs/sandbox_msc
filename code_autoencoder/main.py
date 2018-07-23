e6d44c54be823fdb8f61fe41a13437648ce822ea
a-pouplin <amp115@ic.ac.uk>
2018-07-23 18:03:31 +0100

    test

--- code_autoencoder/main.py
+++ code_autoencoder/main.py
@@ -18,7 +18,7 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--root', default='../mnist/', type=str)
     parser.add_argument('--bs', default=512, type=int)  # batchSize
-    parser.add_argument('--me', default=1, type=int)  # max epoch
+    parser.add_argument('--me', default=10, type=int)  # max epoch
     parser.add_argument('--lr', default=0.005, type=float) # learning rate
     parser.add_argument('--outDir', default='../experiments/', type=str)
     parser.add_argument('--nz', default=2, type=int)  # latent code dimension
